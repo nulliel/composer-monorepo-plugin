13b20361ab0d5ced9589c72006543e3ca18c2140
Stephen Ribich <stephen@ribich.dev>
2020-11-30 20:03:44 +0000

    fix: do not force dev mode when updating sub-package dependencies

--- src/Installer.php
+++ src/Installer.php
@@ -20,8 +20,6 @@ final class Installer
      */
     public function run(array $packages = [], bool $isDev = false, bool $isUpdate = false): int
     {
-        $isDev = $isUpdate ?: $isDev;
-
         if (!$isUpdate && !$this->monorepo->getLocker()->isLocked()) {
             $this->monorepo->io->write("<warning>No lock file found. Updating dependencies instead of installing from lock file</warning>");
             $isUpdate = true;
